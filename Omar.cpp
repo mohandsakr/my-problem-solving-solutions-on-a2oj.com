9d481e0774f4b9e9b78282e2e713f913261d0d27
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-15 20:04:38 +0200

    Add files via upload

---
+++ Omar.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+//problem link :https://a2oj.com/p?ID=1
+// Name        : Omar.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status :accepted
+//============================================================================
+
+#include<iostream>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+int a,b;
+int sum;
+cin>>a>>b;
+sum=a+b;
+cout<<sum<<endl;
+
+}
+return 0;
+
+
+
+}
